1197cca1d98c191d69de0beb648d0e2716811083
NonSwag <mrminecraft00@gmail.com#>
2022-07-15 22:17:09 +0200

    improved PermissionBound.AUTOMATIC checks

--- src/main/java/net/nonswag/tnl/protect/api/flag/bounds/PermissionBound.java
+++ src/main/java/net/nonswag/tnl/protect/api/flag/bounds/PermissionBound.java
@@ -1,12 +1,16 @@
 package net.nonswag.tnl.protect.api.flag.bounds;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permissible;
-import org.bukkit.permissions.ServerOperator;
 
 import javax.annotation.Nonnull;
 
 public interface PermissionBound extends ClassBound<Permissible> {
 
     @Nonnull
-    PermissionBound AUTOMATIC = ServerOperator::isOp;
+    PermissionBound AUTOMATIC = permissible -> {
+        if (!(permissible instanceof Player player)) return permissible.isOp();
+        return permissible.isOp() && player.getGameMode().equals(GameMode.CREATIVE);
+    };
 }
